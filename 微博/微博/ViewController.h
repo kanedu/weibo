dfa9bae07a06610d0d6f65ee68be660344f93516
kanes cp <kanescp@kanesdemacbook-pro.local>
2016-06-12 17:31:06 +0800

    初始化项目

---
+++ 微博/微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微博
+//
+//  Created by kanes cp on 16/6/12.
+//  Copyright © 2016年 kanes cp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 微博/微博/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微博
+//
+//  Created by kanes cp on 16/6/12.
+//  Copyright © 2016年 kanes cp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
